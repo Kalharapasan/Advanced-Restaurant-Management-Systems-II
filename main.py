90a8fa5803f5df781221340679096d17df168b0e
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-25 23:30:05 +0530

    Import all libary in python for project

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+import time
+import datetime
+import tkinter.messagebox
+from tkinter import *
+from tkinter import ttk
+import mysql.connector
+from mysql.connector import Error
+import json
